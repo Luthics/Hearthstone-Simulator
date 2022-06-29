2953aa1c8d67ca71f807bccbe8f553a41461ba4d
Luthics <luthics@foxmail.com>
2022-06-29 23:32:25 +0800

    支持全屏幕显示，自适应调整

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
+// 获取随机颜色的函数
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    //width: 800,
+    //height: 600,
+    scale: {
+        mode: Phaser.Scale.NONE,
+        parent: 'game',
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
     physics: {
         default: 'arcade',
         arcade: {
@@ -16,8 +32,7 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload (){
     this.load.setBaseURL('http://labs.phaser.io');
 
     this.load.image('sky', 'assets/skies/space3.png');
@@ -25,10 +40,7 @@ function preload ()
     this.load.image('red', 'assets/particles/red.png');
 }
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-
+function create (){
     var particles = this.add.particles('red');
 
     var emitter = particles.createEmitter({
@@ -36,8 +48,21 @@ function create ()
         scale: { start: 1, end: 0 },
         blendMode: 'ADD'
     });
+    
+    var sky = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'sky').setOrigin(0);
+    var logo = this.physics.add.image(this.scale.width / 2, this.scale.height / 2, 'logo');
 
-    var logo = this.physics.add.image(400, 100, 'logo');
+    this.scale.on('resize', function (gameSize) {
+
+        var width = gameSize.width;
+        var height = gameSize.height;
+
+        this.cameras.resize(width, height);
+
+        sky.setSize(width, height);
+        logo.setPosition(width / 2, height / 2);
+
+    }, this);
 
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
@@ -45,3 +70,10 @@ function create ()
 
     emitter.startFollow(logo);
 }
+
+//自动更改比例
+window.addEventListener('resize', function (event) {
+
+    game.scale.resize(window.innerWidth, window.innerHeight);
+
+}, false);
