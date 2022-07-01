e83fb5b59e2e5a0c083580658a73cc47045ba31f
Luthics <luthics@foxmail.com>
2022-07-01 18:32:00 +0800

    优化文件结构

---
+++ assets/js/api.js
@@ -0,0 +1,9 @@
+// 获取随机颜色的函数
+    function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
