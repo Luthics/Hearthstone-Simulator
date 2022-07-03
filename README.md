8cdc5ea31a87d3287013b73bb63009aefd32ece8
Luthics <luthics@foxmail.com>
2022-07-03 10:23:19 +0800

    更换背景

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A simulator that lets you simulate Hearthstone matches
 
 To create a simulator that clone Hearthstone
 
+# try to solve the problem
+
 素材来源：https://hs.blizzard.cn/cards
 
 github:https://github.com/Luthics/Hearthstone-Simulator 
--- assets/js/scene.js
+++ assets/js/scene.js
@@ -3,16 +3,11 @@ var config = {
     //width: 800,
     //height: 600,
     scale: {
-        mode: Phaser.Scale.NONE,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         parent: 'game',
         width: window.innerWidth,
         height: window.innerHeight
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
     },
     scene: {
         preload: preload,
@@ -23,27 +18,18 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload (){
-    this.load.setBaseURL('http://labs.phaser.io');
+    //this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('sky', 'assets/imgs/desk.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
 }
 
-function create (){
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-    
+function create (){    
     var sky = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'sky').setOrigin(0);
-    var logo = this.physics.add.image(this.scale.width / 2, this.scale.height / 2, 'logo');
+    var logo = this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'logo');
 
     this.scale.on('resize', function (gameSize) {
-
         var width = gameSize.width;
         var height = gameSize.height;
 
@@ -51,14 +37,7 @@ function create (){
 
         sky.setSize(width, height);
         logo.setPosition(width / 2, height / 2);
-
     }, this);
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
 }
 
 //自动更改比例
